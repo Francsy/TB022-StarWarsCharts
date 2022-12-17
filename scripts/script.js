37dfafb9d0a31fc951b258e2b06b6460906ca015
Francsy <francsyludens@gmail.com>
2022-12-17 13:02:07 +0100

    Add/second exercise is done

--- scripts/script.js
+++ scripts/script.js
@@ -6,89 +6,66 @@
 // // En el eje Y año de publicación
 // // API ENDPOINT --> https://swapi.dev/api/films/
 
-
-/// INSERTAR GRAFICA DESDE LA WEB DE CHARTIST.jS
-
-// Our labels and three data series
-
-
-
-///LOGICA PARA SACAR DATOS DE API, NO TOCAR, FUNCIONA
-
-
 async function makeSWFilmsChart() {
-    let resultado = await fetch("https://swapi.dev/api/films/");
-    let dataBase = await resultado.json();
-    const listaPelis = dataBase.results;
-    
-    const arrayPelis = []; 
-    for (let i = 0; i < listaPelis.length; i++) {
-        arrayPelis.push(listaPelis[i].title);
+  let resultado = await fetch("https://swapi.dev/api/films/");
+  let dataBase = await resultado.json();
+  const filmList = dataBase.results;
+  const arrFilms = filmList.map(element => element.title);
+  const arrAnios = filmList.map(element => element.release_date.substring(0, 4))
+
+
+  var data = {
+    labels: arrFilms,
+    series: [arrAnios]
+  };
+  var options = {
+    showPoint: true,
+    showArea: true,
+    lineSmooth: true,
+    axisX: {
+      offset:80
+    },
+    axisY: {
+      offset: 80,
+      labelInterpolationFnc: function (value) {
+        return '' + value + '';
+      }
     }
-    
-    const arrayFechas = []; 
-    for (let i = 0; i < listaPelis.length; i++) {
-        arrayFechas.push(listaPelis[i].release_date);
-    }
-    
-    // arrayFechas.filter(() => element.length - 6))
-    // arrayFechas.map()).filter((element) => element.length - 6);
-
-    // return console.log(arrayFechas);
-    
-    
-
-    const anios = arrayFechas.map(string => string.substring(0, 4));
-    
-
-    var data = {
-        labels: arrayPelis,
-        series: [anios]
-      };
-      
-      // We are setting a few options for our chart and override the defaults
-      var options = {
-        // Don't draw the line chart points
-        showPoint: true,
-        // Disable line smoothing
-        lineSmooth: false,
-        // X-Axis specific configuration
-        axisX: {
-          // We can disable the grid for this axis
-          showGrid: true,
-          // and also don't show the label
-          showLabel: true
-        },
-        // Y-Axis specific configuration
-        axisY: {
-          // Lets offset the chart a bit from the labels
-          offset: 60,
-          // The label interpolation function enables you to modify the values
-          // used for the labels on each axis. Here we are converting the
-          // values into million pound.
-          labelInterpolationFnc: function(value) {
-            return '' + value + '';
-          }
-        }
-      };
-      
-      // All you need to do is pass your configuration as third parameter to the chart function
-      new Chartist.Line('.ct-chart', data, options);
+  };
+  new Chartist.Line('#SWFilms', data, options);
 }
+makeSWFilmsChart();
 
-makeSWFilmsChart()
-
-
-
-
-
-
-
-// Pediremos los personajes de Star Wars y pintaremos una gráfica de barras en la que podamos ver
-// En el eje X el nombre del personaje
-// En el eje Y el número de películas en las que ha participado.
-// API ENDPOINT --> https://swapi.dev/api/people/
-// Para pintar todo esto usaremos Chartist Link a la docu: 
+// // Pediremos los personajes de Star Wars y pintaremos una gráfica de barras en la que podamos ver
+// // En el eje X el nombre del personaje
+// // En el eje Y el número de películas en las que ha participado.
+// // API ENDPOINT --> https://swapi.dev/api/people/
+// // Para pintar todo esto usaremos Chartist Link a la docu: 
 
 
-// https://swapi.dev/api/people/
+async function getCharacters() {
+  let results = await fetch("https://swapi.dev/api/people/");
+  let charactersData = await results.json();
+  const charactersList = charactersData.results;
+  let arrNames = charactersList.map(element => element = element.name);
+  let arrNumFilms = charactersList.map(element => element.films.length);
+  
+  new Chartist.Bar('#SWCharacters', {
+    labels: arrNames,
+    series: [arrNumFilms]
+  }, {
+    stackBars: true,
+    axisY: {
+      labelInterpolationFnc: function(value) {
+        return (value) + '';
+      }
+    }
+  }).on('draw', function(data) {
+    if(data.type === 'bar') {
+      data.element.attr({
+        style: 'stroke-width: 30px'
+      });
+    }
+  });
+}
+getCharacters()
